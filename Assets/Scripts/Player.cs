20a785d32c554ec7c50f9af36fd48d5c06029dd6
Charles Hart <hartcp8@gmail.com>
2018-07-21 00:55:03 -0400

    Fixed movement / sound bug

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -22,7 +22,6 @@ namespace Completed
         public AudioClip gameOverSound;             //Audio clip to play when player dies.
         private bool playerMoving = false;
         private Animator animator;                  //Used to store a reference to the Player's animator component.
-        private int food;                           //Used to store player food points total during level.
 
         public AudioSource Step;
         public AudioSource BlockedMovement;
@@ -159,13 +158,14 @@ namespace Completed
 
             if (canMove)
             {
-                Step.Play();
+                if (!playerMoving)
+                {
+                    Step.Play();
+                }
                 playerMoving = true;
             }
 
-
-            //Since the player has moved and lost food points, check if the game has ended.
-            CheckIfGameOver();
+            
 
             //Set the playersTurn boolean of GameManager to false now that players turn is over.
             GameManager.instance.playersTurn = false;
@@ -179,14 +179,6 @@ namespace Completed
         protected override void OnCantMove()
         {
             BlockedMovement.Play();
-            //Set hitWall to equal the component passed in as a parameter.
-            //Wall hitWall = component as Wall;
-
-            ////Call the DamageWall function of the Wall we are hitting.
-            //hitWall.DamageWall(wallDamage);
-
-            ////Set the attack trigger of the player's animation controller in order to play the player's attack animation.
-            //animator.SetTrigger("playerChop");
         }
 
         protected override void OnFinishedMoving()
@@ -195,52 +187,6 @@ namespace Completed
         }
 
 
-        //OnTriggerEnter2D is sent when another object enters a trigger collider attached to this object (2D physics only).
-        private void OnTriggerEnter2D(Collider2D other)
-        {
-            //Check if the tag of the trigger collided with is Exit.
-            //if (other.tag == "Exit")
-            //{
-            //    //Invoke the Restart function to start the next level with a delay of restartLevelDelay (default 1 second).
-            //    Invoke("Restart", restartLevelDelay);
-
-            //    //Disable the player object since level is over.
-            //    enabled = false;
-            //}
-
-            ////Check if the tag of the trigger collided with is Food.
-            //else if (other.tag == "Food")
-            //{
-            //    //Add pointsPerFood to the players current food total.
-            //    food += pointsPerFood;
-
-            //    //Update foodText to represent current total and notify player that they gained points
-            //    foodText.text = "+" + pointsPerFood + " Food: " + food;
-
-            //    //Call the RandomizeSfx function of SoundManager and pass in two eating sounds to choose between to play the eating sound effect.
-            //    SoundManager.instance.RandomizeSfx(eatSound1, eatSound2);
-
-            //    //Disable the food object the player collided with.
-            //    other.gameObject.SetActive(false);
-            //}
-
-            ////Check if the tag of the trigger collided with is Soda.
-            //else if (other.tag == "Soda")
-            //{
-            //    //Add pointsPerSoda to players food points total
-            //    food += pointsPerSoda;
-
-            //    //Update foodText to represent current total and notify player that they gained points
-            //    foodText.text = "+" + pointsPerSoda + " Food: " + food;
-
-            //    //Call the RandomizeSfx function of SoundManager and pass in two drinking sounds to choose between to play the drinking sound effect.
-            //    SoundManager.instance.RandomizeSfx(drinkSound1, drinkSound2);
-
-            //    //Disable the soda object the player collided with.
-            //    other.gameObject.SetActive(false);
-            //}
-        }
-
 
         //Restart reloads the scene when called.
         private void Restart()
@@ -248,42 +194,6 @@ namespace Completed
             //Load the last scene loaded, in this case Main, the only scene in the game. And we load it in "Single" mode so it replace the existing one
             //and not load all the scene object in the current scene.
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex, LoadSceneMode.Single);
-        }
-
-
-        //LoseFood is called when an enemy attacks the player.
-        //It takes a parameter loss which specifies how many points to lose.
-        //public void LoseFood(int loss)
-        //{
-        //    //Set the trigger for the player animator to transition to the playerHit animation.
-        //    animator.SetTrigger("playerHit");
-
-        //    //Subtract lost food points from the players total.
-        //    food -= loss;
-
-        //    //Update the food display with the new total.
-        //    foodText.text = "-" + loss + " Food: " + food;
-
-        //    //Check to see if game has ended.
-        //    CheckIfGameOver();
-        //}
-
-
-        //CheckIfGameOver checks if the player is out of food points and if so, ends the game.
-        private void CheckIfGameOver()
-        {
-            //Check if food point total is less than or equal to zero.
-            //if (food <= 0)
-            //{
-            //    //Call the PlaySingle function of SoundManager and pass it the gameOverSound as the audio clip to play.
-            //    SoundManager.instance.PlaySingle(gameOverSound);
-
-            //    //Stop the background music.
-            //    SoundManager.instance.musicSource.Stop();
-
-            //    //Call the GameOver function of GameManager.
-            //    GameManager.instance.GameOver();
-            //}
         }
     }
 }
